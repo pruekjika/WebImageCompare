c191cc4a56f4f0b5386af9058286aa3b06653227
meangpu <51825872+meangpu@users.noreply.github.com>
2024-05-13 19:39:40 +0700

    stable

--- src/Components/what.tsx
+++ src/Components/what.tsx
@@ -1,18 +1,18 @@
 import React, { useEffect } from "react";
+import ExifReader from "exifreader";
 
 const What = async () => {
   // "https://raw.githubusercontent.com/pruekjika/GardenImgDB/main/ImageDB/2.webp";
   // "https://raw.githubusercontent.com/pruekjika/GardenImg360/main/low-2.jpg";
 
-  const blob = await fetch(
-    "https://raw.githubusercontent.com/pruekjika/GardenImg360/main/low-2.jpg"
-  ).then((r) => r.blob()); //creating blob object
+  const tags = await ExifReader.load(
+    "https://raw.githubusercontent.com/pruekjika/GardenImgDB/main/ImageDB/2.webp"
+  );
 
-  const file = new File([blob], "cover.jpg", {
-    type: "image/jpg",
-  });
+  const imageDate = tags["DateTimeOriginal"].description;
+  // const unprocessedTagValue = tags["DateTimeOriginal"].value;
+  console.log(imageDate);
 
-  console.log(file);
   return <div>what</div>;
 };
 
