d76dff8eda6e652ba6e145e9cd9fd0b590443a8e
meangpu <51825872+meangpu@users.noreply.github.com>
2024-05-13 12:56:19 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ function App() {
   return (
     <>
       {/* image gallery if final call because it easier to send image button data to compare */}
+      <h1>Image compare</h1>
       <ImageGallery images={images} />
     </>
   );
